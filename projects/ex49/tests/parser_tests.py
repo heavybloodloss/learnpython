90ad18e86764d2356f05160b859f9f68696a0ecd
heavybloodloss <47472573+heavybloodloss@users.noreply.github.com>
2020-11-16 15:26:34 -0500

    tests for ex49
    writing and testing tests for ex49

---
+++ projects/ex49/tests/parser_tests.py
@@ -0,0 +1,30 @@
+from nose.tools import *
+from ex49 import parser
+
+
+def test_peek():
+    pass
+
+
+def test_match():
+    pass
+
+
+def test_skip():
+    pass
+
+
+def test_parse_verb():
+    pass
+
+
+def test_parse_object():
+    pass
+
+
+def test_parse_subject():
+    pass
+
+
+def test_parse_sentence():
+    pass
