fc7fe0ea3418ad8959956f0e4cbd5494ac186b77
heavybloodloss <47472573+heavybloodloss@users.noreply.github.com>
2020-11-18 15:58:47 -0500

    exercise 50 stuff

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -6,10 +6,12 @@ urls = (
 
 app = web.application(urls, globals())
 
-class index:
+render = web.template.render('templates/')
+
+class Index(object):
     def GET(self):
         greeting = "Hello World"
-        return greeting
+        return render.index(greeting = greeting)
 
 if __name__ == '__main__':
     app.run()
